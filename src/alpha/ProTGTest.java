09b1fa6b11d8053d217e169dfa5e247d520d80c4
Lukas Rose <lukas.rose@gmx.de>
2015-09-10 20:00:04 +0200

    Further Steps

--- src/alpha/ProTGTest.java
+++ src/alpha/ProTGTest.java
@@ -1,11 +1,45 @@
 package alpha;
 
+import org.telegram.api.TLAbsUpdates;
+import org.telegram.api.engine.ApiCallback;
+import org.telegram.api.engine.AppInfo;
+import org.telegram.api.engine.TelegramApi;
 import org.telegram.mtproto.pq.Authorizer;
 
 public class ProTGTest {
 	
 	
 	public static void main(String[] args) {
-		
+		TelegramApi api = new TelegramApi(new MyAbsApiState(), new AppInfo(14929, "myDeviceModel", "mySystemVersion", "myAppVersion", "myLangCode"), new ApiCallback(){ //Error: Cannot instantiate ApiCallback, opening { seems to fix. [1]
+			
+			//@Override //Proposed by [1], but doesn't work here (?)
+			public void onApiDies(TelegramApi api){
+				// When auth key or user authorization dies
+				System.out.println("Api died");
+			}
+			
+			@Override //Proposed by [1], but seemed to work without (?)
+			public void onUpdatesInvalidated(TelegramApi api){
+				// When api engine expects that update sequence might be broken
+				System.out.println("Update sequence broken? Updates Invalidated.");
+			}
+
+			@Override
+			public void onAuthCancelled(TelegramApi api) { //These methods are not present in [1], but proposed by eclipse (implement missing methods)
+				System.out.println("Auth cancelled");				
+			}
+
+			@Override
+			public void onUpdate(TLAbsUpdates updates) { //These methods are not present in [1], but proposed by eclipse (implement missing methods)
+				System.out.println("Auth cancelled");
+			}
+		});
+//		MyApiStorage apis = new MyApiStorage();
+		MyAbsApiState apiState = new MyAbsApiState();
+		AppInfo ai = new AppInfo(14929, "myDeviceModel", "mySystemVersion", "myAppVersion", "myLangCode");
 	}
 }
+
+/*
+ * [1] https://github.com/ex3ndr/telegram-api#rpc-calls
+ */
